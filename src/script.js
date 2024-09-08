fdc6f94cecbc9cfe238be90d0c721daecdd72069
Philipp Wischnewski <phil-wisch@web.de>
2024-09-08 23:25:53 +0200

    block stacking works

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 const ROWS = 24;
 const COLUMNS = 10;
 
+let gameRunning = false;
+
 let score;
-let gameboard;
+let gameBoard;
 let timer;
 
 const gameboardElement = document.querySelector('#gameboard');
@@ -22,30 +24,51 @@ function setup() {
 }
 
 function startGame() {
-    score = 0;
-
-
-    timer = setInterval(gameTick, 10);
-    gameboardDivs.forEach(div => div.style = 'background-color: white;');
+    if (!gameRunning) {
+        gameRunning = true;
+        score = 0;
+        gameBoard = new Board();
+        timer = setInterval(gameTick, 100);
+        gameboardDivs.forEach(div => div.style = 'background-color: white;');
+    }
 }
 
 function endGame() {
-    clearInterval(timer);
+    if (gameRunning) {
+        gameRunning = false;
+        clearInterval(timer);
+    }
 }
 
 let fallingPiece;
 function gameTick() {
+    // check for collisions
+    if (fallingPiece) {
+        // hitting the floor
+        const floor_collision = fallingPiece.positions.some(position => position.y === ROWS - 1);
+        // if there is a block DIRECTLY beneath the piece (actually any of the blocks the piece is made out of)
+        const collision = fallingPiece.positions.some(position => gameBoard.isOccupied(new Position(position.x, position.y + 1)));
+
+        // the piece can't move down any more and stays where it currently is.
+        if (floor_collision || collision) {
+            fallingPiece.positions.forEach(position => {
+                console.log(position);
+                gameBoard.addBlock(position);
+            });
+            fallingPiece = null;
+            return;
+        }
+    }
+
     // if there is no falling piece, spawn a new one
     if (!fallingPiece) {
         fallingPiece = new Piece();
-        draw(fallingpiece.color, fallingPiece.position);
+        draw(fallingPiece.color, fallingPiece.positions);
     } else { // if there is a falling piece, move it down
-        draw('white', fallingPiece.position);
+        draw('white', fallingPiece.positions);
         fallingPiece.moveDown();
-        draw(fallingPiece.color, fallingPiece.position);
+        draw(fallingPiece.color, fallingPiece.positions);
     }
-
-    // check if the falling piece hit the ground
 }
 
 function draw(color, positions) {
@@ -54,22 +77,21 @@ function draw(color, positions) {
             throw new Error('Position out of bounds');
         }
         gameboardDivs[position.y * 10 + position.x].style = `background-color: ${color}`;
-        console.log(position);
     })
 }
 
 class Piece {
-    position;
+    positions;
     color;
     constructor() {
         // TODO pick a random piece
         // for now only THE BEST PIECE 4 down
-        this.position = [new Position(0, 1), new Position(0, 0), new Position(0, 2), new Position(0, 3)];
-        color = 'blue';
+        this.positions = [new Position(0, 1), new Position(0, 0), new Position(0, 2), new Position(0, 3)];
+        this.color = 'blue';
     }
 
     moveDown() {
-        this.position.forEach(position => position.y += 1);
+        this.positions.forEach(position => position.y += 1);
     }
 }
 
@@ -77,6 +99,10 @@ class Position {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+    }
+
+    toString() {
+        return `${this.x},${this.y}`;
     }
 }
 
@@ -93,7 +119,7 @@ class Board {
         this.occupiedPositions.delete(position.toString());
     }
 
-    check(position) {
+    isOccupied(position) {
         return this.occupiedPositions.has(position.toString());
     }
 }
